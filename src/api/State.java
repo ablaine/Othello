b455b5e21256dc7d72b9145873948df59cd832e8
Andrew Blaine <ablaine@ups.edu>
2009-08-26 16:45:43 -0700

    Documented the State class.

--- src/api/State.java
+++ src/api/State.java
@@ -3,14 +3,25 @@ package api;
 import java.util.Random;
 
 /**
+ * This enum represents one of three states an Othello board tile can be, a
+ * dark piece, a light piece or an empty spot. This class is additionally used
+ * to associate players with which pieces they own (e.g. light and dark).
  *
- * @author ablaine
+ * @author Andrew Blaine
  */
 public enum State {
 	EMPTY, LIGHT, DARK;
 
 	private final static Random random = new Random();
 
+	/**
+	 * If this instance is <code>LIGHT</code>, this returns <code>DARK</code>,
+	 * and vice versa. However, if this instance is <code>EMPTY</code>, this
+	 * returns itself.
+	 * 
+	 * @return the opposite state, or <code>EMPTY</code> if that is the current
+	 * instance
+	 */
 	public State getOpposite() {
 		switch(this) {
 			case LIGHT: return DARK;
@@ -19,6 +30,12 @@ public enum State {
 		}
 	}
 
+	/**
+	 * Returns a random instance of either <code>LIGHT</code> or
+	 * <code>DARK</code>.
+	 * 
+	 * @return either <code>LIGHT</code> or <code>DARK</code>, picked at random
+	 */
 	public static State getRandom() {
 		if (random.nextInt(2) == 0) {
 			return DARK;
