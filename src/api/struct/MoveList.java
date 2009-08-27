8c3bd364fb3637dac10db574aed0ef0b6b91d6a3
Andrew Blaine <ablaine@ups.edu>
2009-08-26 20:01:51 -0700

    Documented the MoveList class.

--- src/api/struct/MoveList.java
+++ src/api/struct/MoveList.java
@@ -5,11 +5,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- *
- * @author ablaine
+ * A simple <code>Move</code> oriented list.
+ * 
+ * @author Andrew Blaine
  */
 public final class MoveList extends LinkedList<Move> {
 
+	/**
+	 * Extracts just the locations from the <code>Move</code> elements and
+	 * returns the list.
+	 *
+	 * @return a list of just the locations
+	 */
 	public List<Point> toPoints() {
 		List<Point> result = new LinkedList<Point>();
 		for (Move m : this) {
